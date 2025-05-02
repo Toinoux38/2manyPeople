f59cd8fc39700af541f78eddd34ff50b45745dec
toinoux38 <antoinehoullier555@gmail.com>
2025-05-02 05:58:12 -0700

    Gradle kts edit for javafx

--- 2manyPeople/build.gradle.kts
+++ 2manyPeople/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "org.example"
@@ -10,9 +11,12 @@ repositories {
     mavenCentral()
 }
 
+javafx {
+    version = "17.0.2"
+    modules = listOf("javafx.controls", "javafx.fxml")
+}
+
 dependencies {
-    implementation("org.openjfx:javafx-controls:17.0.2")
-    implementation("org.openjfx:javafx-fxml:17.0.2")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }

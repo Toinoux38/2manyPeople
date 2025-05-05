c42dbeecf96288e1519c1a7b7d87018ae45f89ea
a.houllier@eleve.leschartreux.net <a.houllier@eleve.leschartreux.net>
2025-05-05 22:12:28 +0200

    added default super constructor to avoid errors

--- 2manyPeople/src/main/java/com/citybuilder/modelBis/events/StartEvent.java
+++ 2manyPeople/src/main/java/com/citybuilder/modelBis/events/StartEvent.java
@@ -1,4 +1,7 @@
 package com.citybuilder.modelBis.events;
 
 public class StartEvent extends GameEvent {
+    protected StartEvent(String type, int x, int y) {
+        super(type, x, y);
+    }
 }
--- 2manyPeople/src/main/java/com/citybuilder/modelBis/events/UpdateEvent.java
+++ 2manyPeople/src/main/java/com/citybuilder/modelBis/events/UpdateEvent.java
@@ -7,6 +7,7 @@ public class UpdateEvent extends GameEvent {
     public final Cell[][] map;
 
     public UpdateEvent(Cell[][] map) {
+        super("update", 0, 0);
         this.map = map;
     }
 }

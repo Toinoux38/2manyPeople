fe676b23faae3edccf13fa88ea2a4b5a73dcce49
toinoux38 <antoinehoullier555@gmail.com>
2025-05-05 22:06:13 -0700

    [Not clean] added gameevent cellplaced/removed

--- 2manyPeople/src/main/java/com/citybuilder/modelBis/events/GameEvent.java
+++ 2manyPeople/src/main/java/com/citybuilder/modelBis/events/GameEvent.java
@@ -1,3 +1,25 @@
 package com.citybuilder.modelBis.events;
 
-public abstract class GameEvent {}
+public abstract class GameEvent {
+    private final String type;
+    private final int x;
+    private final int y;
+
+    protected GameEvent(String type, int x, int y) {
+        this.type = type;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
